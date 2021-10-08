04c1b0de6afa5c1e76de41869ec5f46af98de640
mohammed ahmedin <mohammed.ahmedin@outlook.com>
2021-10-08 13:56:40 +0200

    first commit

--- functions.php
+++ functions.php
@@ -21,3 +21,9 @@ function hello_elementor_child_enqueue_scripts() {
 	);
 }
 add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts', 20 );
+
+function remove_actions() {
+	remove_action('woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail');
+	}
+	// add_filter( 'woocommerce_enqueue_styles', '__return_false' );
+	add_action('init', 'remove_actions');
